522b266e1ec067e766e2d7843ff90530ea85c788
Ryan Davis <ryandavis.au@gmail.com>
2019-09-28 15:48:29 +1000

    Bump addin version to 2.1.0

--- Continuous.Client.MonoDevelop/Properties/AddinInfo.cs
+++ Continuous.Client.MonoDevelop/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"Continuous.Client.MonoDevelop", 
 	Namespace = "Continuous.Client.MonoDevelop",
-	Version = "2.0.6"
+	Version = "2.1.0"
 )]
 
 [assembly:AddinName ("Continuous Coding")]
